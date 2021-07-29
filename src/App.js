3049522c17da3bec2d9288af5644306440217125
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2021-07-28 23:42:57 -0500

    mini

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from "./config.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/banquete_logo.svg" alt="logo" />
         <p className="App-link">PROXIMAMENTE</p>
-        <p>Sitio en construcción</p>
       </header>
     </div>
   );
